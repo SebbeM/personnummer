6f3aa63c62bfc3c3b3224e63f9b3bf81d3a013c1
Sebastian Magnusson <sebbe.magnusson@gmail.com>
2020-06-03 16:32:06 +0200

    Initial commit

---
+++ src/com/java/Main.java
@@ -0,0 +1,13 @@
+package com.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static boolean validityCheck(String s) {
+
+        return false;
+    }
+}
